5768b6ba9c812cbb34537d914a2cac288da137cb
masdefry <muhammaddefryan@gmail.com>
2021-09-24 15:26:19 +0700

    React Navigation OH 2

--- src/Screens/Navigation/Navigation.js
+++ src/Screens/Navigation/Navigation.js
@@ -8,6 +8,7 @@ const StackNavigation = () => {
         <Stack.Navigator>
             <Stack.Screen name="Login" component={Login} />
             <Stack.Screen name="Register" component={Register} />
+            <Stack.Screen name="HomeStackNavigation" component={HomeStackNavigation} />
         </Stack.Navigator>
     )
 }
@@ -23,6 +24,9 @@ const Login = ({ navigation: {navigate} }) => {
             <Button title="On Change Data" onPress={ () => setDataLogin([1, 'ryandefryan']) } />
             <Text onPress={() => navigate('Register', { sendData: dataLogin })} style={{ color: 'blue' }}>
                 Go to Register
+            </Text>
+            <Text onPress={() => navigate('HomeStackNavigation', {sendData: dataLogin})} style={{ color: 'blue' }}>
+                Go to Home
             </Text>
         </View>
     )
@@ -47,4 +51,82 @@ const Register = ({route}) => {
     )
 }
 
+// ##########
+const HomeStack = createStackNavigator()
+const HomeStackNavigation = ({route}) => {
+    return(
+        <HomeStack.Navigator>
+            <HomeStack.Screen name='Home' component={Home} initialParams={{ dataFromLogin: route.params.sendData }} />
+            <HomeStack.Screen name='Products' component={Products} />
+            <HomeStack.Screen name='Cart' component={Cart} />
+            <HomeStack.Screen name='Transaction' component={Transaction} />
+        </HomeStack.Navigator>
+    )
+}
+
+// Screen Home
+const Home = ({route}) => {
+
+    useEffect(() => {
+        console.log(route.params)
+    }, [])
+
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen Home
+            </Text>
+            <Text style={{ fontStyle: 'italic', color: 'blue' }}>
+                Go to Products
+            </Text>
+            <Text style={{ fontStyle: 'italic', color: 'blue' }}>
+                Go to Cart
+            </Text>
+            <Text style={{ fontStyle: 'italic', color: 'blue' }}>
+                Go to Transaction
+            </Text>
+        </View>
+    )
+}
+
+// Screen Products
+const Products = () => {
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen Products
+            </Text>
+        </View>
+    )
+}
+
+// Screen Cart
+const Cart = () => {
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen Cart
+            </Text>
+        </View>
+    )
+}
+
+// Screen Transaction
+const Transaction = () => {
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen Transaction
+            </Text>
+        </View>
+    )
+}
+
 export default StackNavigation
+
+// Login
+// Register
+// Home
+    // Products
+    // Cart
+    // Transaction

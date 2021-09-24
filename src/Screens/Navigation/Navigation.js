f8471299440a9e247050d540cc00a8964cb2fa10
masdefry <muhammaddefryan@gmail.com>
2021-09-24 20:19:02 +0700

    React Navigation AH 1

--- src/Screens/Navigation/Navigation.js
+++ src/Screens/Navigation/Navigation.js
@@ -1,31 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import { createStackNavigator } from '@react-navigation/stack'
-import { View, Text, Button } from 'react-native';
+import React from 'react';
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { View, Text } from 'react-native';
+import { useEffect } from 'react/cjs/react.development';
 
 const Stack = createStackNavigator()
 const StackNavigation = () => {
     return(
         <Stack.Navigator>
-            <Stack.Screen name="Login" component={Login} />
-            <Stack.Screen name="Register" component={Register} />
-            <Stack.Screen name="HomeStackNavigation" component={HomeStackNavigation} />
+            <Stack.Screen name='Login' component={Login} />
+            <Stack.Screen name='Register' component={Register} />
+            <Stack.Screen name='BottomStackNavigation' component={BottomStackNavigation} />
         </Stack.Navigator>
     )
 }
 
 // Screen Login
 const Login = ({ navigation: {navigate} }) => {
-    const [dataLogin, setDataLogin] = useState([1, 'mdefryan'])
+
+    let nama = 'Defryan'
+
     return(
         <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
             <Text>
                 This is Screen Login
             </Text>
-            <Button title="On Change Data" onPress={ () => setDataLogin([1, 'ryandefryan']) } />
-            <Text onPress={() => navigate('Register', { sendData: dataLogin })} style={{ color: 'blue' }}>
-                Go to Register
+            <Text onPress={ () => navigate('Register', {sendData: nama}) } style={{ color: 'blue' }}>
+                Go to Register Screen
             </Text>
-            <Text onPress={() => navigate('HomeStackNavigation', {sendData: dataLogin})} style={{ color: 'blue' }}>
+            <Text onPress={ () => navigate('BottomStackNavigation') } style={{ color: 'green' }}>
                 Go to Home
             </Text>
         </View>
@@ -33,7 +36,7 @@ const Login = ({ navigation: {navigate} }) => {
 }
 
 // Screen Register
-const Register = ({route}) => {
+const Register = ({ navigation: {navigate}, route }) => {
 
     useEffect(() => {
         console.log(route.params.sendData)
@@ -44,47 +47,38 @@ const Register = ({route}) => {
             <Text>
                 This is Screen Register
             </Text>
-            <Text style={{ fontStyle: 'italic', color: 'red' }}>
-                Data from Screen Login: {route.params.sendData[1]}
+            <Text style={{ color: 'red' }}>
+                Data from Login Screen: {route.params.sendData}
+            </Text>
+            <Text onPress={() => navigate('Login')} style={{ color: 'blue' }}>
+                Back to Login Screen
             </Text>
         </View>
     )
 }
 
 // ##########
-const HomeStack = createStackNavigator()
-const HomeStackNavigation = ({route}) => {
+const BottomStack = createBottomTabNavigator()
+const BottomStackNavigation = () => {
     return(
-        <HomeStack.Navigator>
-            <HomeStack.Screen name='Home' component={Home} initialParams={{ dataFromLogin: route.params.sendData }} />
-            <HomeStack.Screen name='Products' component={Products} />
-            <HomeStack.Screen name='Cart' component={Cart} />
-            <HomeStack.Screen name='Transaction' component={Transaction} />
-        </HomeStack.Navigator>
+        <BottomStack.Navigator>
+            <BottomStack.Screen name="Products" component={Products} />
+            <BottomStack.Screen name="HomeStackNavigation" component={HomeStackNavigation} />
+            <BottomStack.Screen name="Transaction" component={Transaction} />
+        </BottomStack.Navigator>
     )
 }
 
 // Screen Home
-const Home = ({route}) => {
-
-    useEffect(() => {
-        console.log(route.params)
-    }, [])
-
+const Home = ({ navigation: {navigate} }) => {
     return(
         <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
             <Text>
                 This is Screen Home
             </Text>
-            <Text style={{ fontStyle: 'italic', color: 'blue' }}>
-                Go to Products
-            </Text>
-            <Text style={{ fontStyle: 'italic', color: 'blue' }}>
-                Go to Cart
-            </Text>
-            <Text style={{ fontStyle: 'italic', color: 'blue' }}>
-                Go to Transaction
-            </Text>
+            <Text onPress={() => navigate('A')}>
+                Go to Screen A
+            </Text>    
         </View>
     )
 }
@@ -95,17 +89,6 @@ const Products = () => {
         <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
             <Text>
                 This is Screen Products
-            </Text>
-        </View>
-    )
-}
-
-// Screen Cart
-const Cart = () => {
-    return(
-        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
-            <Text>
-                This is Screen Cart
             </Text>
         </View>
     )
@@ -122,11 +105,63 @@ const Transaction = () => {
     )
 }
 
+// ###############
+
+const HomeStack = createStackNavigator()
+const HomeStackNavigation = () => {
+    return(
+        <HomeStack.Navigator>
+        <HomeStack.Screen name='Home' component={Home} />
+        <HomeStack.Screen name='A' component={A} />
+        <HomeStack.Screen name='B' component={B} />
+        <HomeStack.Screen name='C' component={C} />
+    </HomeStack.Navigator>
+    )
+}
+
+// Screen A
+const A = () => {
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen A
+            </Text>
+        </View>
+    )
+}
+
+// Screen B
+const B = () => {
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen B
+            </Text>
+        </View>
+    )
+}
+
+// Screen C
+const C = () => {
+    return(
+        <View style={{ height: '100%', justifyContent: 'center', alignItems: 'center' }}>
+            <Text>
+                This is Screen C
+            </Text>
+        </View>
+    )
+}
+
 export default StackNavigation
 
 // Login
 // Register
-// Home
+    // Home
+        // A
+            // A1
+            // A2
+            // A3
+        // B
+        // C
     // Products
-    // Cart
     // Transaction
